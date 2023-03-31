66518164c47f6313829e8a25c9870cae8dad1bc4
asemyamak <asemyamak@yahoo.com>
2023-03-31 15:46:42 +0300

    Add drag and drop adn Template

--- Web Dev/Home/home.js
+++ Web Dev/Home/home.js
@@ -1,6 +1,4 @@
-
-if (window.addEventListener("DOMContentLoaded", function()
-{
+document.addEventListener('DOMContentLoaded', function() {
     const myDiv = document.querySelector(".container");
     const menu = document.querySelector(".open_menu");
     myDiv.addEventListener("click", function() {
@@ -8,5 +6,11 @@ if (window.addEventListener("DOMContentLoaded", function()
     menu.classList.toggle("open_menu_content");
     console.log("toggling menu is working");
                                             });
+});
 
-}));
+// Check if the clicked element is a button with the "myButton" class
+// if (event.target.classList.contains('myButton')) {
+
+
+//       if (event.ctrlKey) {  working  Checks if control is being pressed.
+//       console.log('working');
